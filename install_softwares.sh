3ad8a9ee037b323b745b014540accf492c11fc0d
Elias Sun <eliassun@gmail.com>
2022-12-16 23:58:53 -0800

    freeswitch nat for aws

--- install_softwares.sh
+++ install_softwares.sh
@@ -2,22 +2,55 @@
 
 cd /home/ubuntu
 mkdir install
-cd install
-sudo apt-get update > update.log
-sudo apt-get -y upgrade > upgrade.log
-sudo apt-get -y install strongswan strongswan-pki libcharon-extra-plugins libcharon-extauth-plugins libstrongswan-extra-plugins libtss2-tcti-tabrmd0 net-tools > strong.log
-echo "installing certs..." > certs.log
-sudo ipsec pki --gen --size 4096 --type rsa --outform pem > /etc/ipsec.d/private/ca.key.pem
-sudo ipsec pki --self --in /etc/ipsec.d/private/ca.key.pem --type rsa --dn "CN=site1@elias" --ca --lifetime 3650 --outform pem ca.cert.pem > /etc/ipsec.d/cacerts/ca.cert.pem
-sudo ipsec pki --gen --size 4096 --type rsa --outform pem > /etc/ipsec.d/private/server.key.pem
-IP=`curl http://checkip.amazonaws.com`
-echo $IP > ip.log
-sudo ipsec pki --pub --in /etc/ipsec.d/private/server.key.pem --type rsa | ipsec pki --issue --lifetime 3650 --cacert /etc/ipsec.d/cacerts/ca.cert.pem --cakey /etc/ipsec.d/private/ca.key.pem --dn "CN=<$IP>" --san="<$IP>" --flag serverAuth --flag ikeIntermediate --outform pem > /etc/ipsec.d/certs/server.cert.pem
-echo "enable forwarding..." > forward.log
-sudo echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
-sudo echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
-sudo echo "net.ipv4.conf.all.accept_redirects = 0" >>  /etc/sysctl.conf
-sudo echo "net.ipv6.conf.all.accept_redirects = 0" >>  /etc/sysctl.conf
-sudo sysctl -p
-echo "Done" > done.log
+
+sudo apt-get install -y libavformat-dev
+sudo apt-get install -y libswscale-dev
+sudo apt-get install -y libpq-dev
+sudo apt -y update
+sudo apt install -y git subversion build-essential autoconf automake libtool libncurses5 libncurses5-dev make libjpeg-dev libtool libtool-bin libsqlite3-dev libpcre3-dev libspeexdsp-dev libldns-dev libedit-dev yasm liblua5.2-dev libopus-dev cmake
+sudo apt install -y libcurl4-openssl-dev libexpat1-dev libgnutls28-dev libtiff5-dev libx11-dev unixodbc-dev libssl-dev python-dev zlib1g-dev libasound2-dev libogg-dev libvorbis-dev libperl-dev libgdbm-dev libdb-dev uuid-dev libsndfile1-dev
+
+cd /usr/src
+sudo git clone https://github.com/signalwire/libks.git
+cd libks
+sudo cmake .
+sudo make > /home/ubuntu/install/libks.make.log
+sudo make install
+
+cd /usr/src
+sudo git clone https://github.com/signalwire/signalwire-c.git
+cd signalwire-c
+sudo cmake .
+sudo make > /home/ubuntu/install/signalwire-c.make.log
+sudo make install
+
+cd /usr/src
+sudo wget https://files.freeswitch.org/freeswitch-releases/freeswitch-1.10.3.-release.zip
+sudo apt -y install unzip
+sudo unzip freeswitch-1.10.3.-release.zip
+cd freeswitch-1.10.3.-release/
+
+sudo ./configure -C > /home/ubuntu/install/freeswitch.configure-c.log
+sudo make >  /home/ubuntu/install/freeswitch.make.log
+
+sudo make install
+
+sudo make all cd-sounds-install cd-moh-install >  /home/ubuntu/install/sound.make.log
+sudo ln -s /usr/local/freeswitch/bin/freeswitch /usr/bin/
+sudo ln -s /usr/local/freeswitch/bin/fs_cli /usr/bin
+cd /usr/local
+
+sudo groupadd freeswitch
+sudo adduser --disabled-password  --quiet --system --home /usr/local/freeswitch --gecos "FreeSWITCH Voice/Video Service" --ingroup freeswitch freeswitch
+sudo chown -R freeswitch:freeswitch /usr/local/freeswitch/
+sudo chmod -R ug=rwX,o= /usr/local/freeswitch/
+sudo chmod -R u=rwx,g=rx /usr/local/freeswitch/bin/
+
+cd  /home/ubuntu/install
+sudo wget https://eliassun.github.io/freeswitch.service
+sudo cp freeswitch.service /etc/systemd/system/freeswitch.service
+sudo chmod ugo+x /etc/systemd/system/freeswitch.service
+sudo systemctl start freeswitch.service
+sudo systemctl enable freeswitch.service
+sudo systemctl status freeswitch.service > freeswitch.status
 
